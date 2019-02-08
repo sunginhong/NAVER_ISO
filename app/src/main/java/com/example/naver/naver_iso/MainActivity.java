d432bf6cf2ecb4379b46b78f4d4d4ccd92d291b1
sunginhong <nicehsi@naver.com>
2019-02-08 11:35:22 +0900

    190208__2

--- app/src/main/java/com/example/naver/naver_iso/MainActivity.java
+++ app/src/main/java/com/example/naver/naver_iso/MainActivity.java
@@ -120,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
-        
+
         // 앱 실행시 체크
         String getNetwork =  getWhatKindOfNetwork(getApplication());
         if(getNetwork.equals("NONE")){ newtwork = false; }
